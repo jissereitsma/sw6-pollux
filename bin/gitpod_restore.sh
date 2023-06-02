a47869fabb7120cd1d25f6a3918847b420fbf16b
jisse Reitsma <jisse@yireo.com>
2023-06-02 09:16:19 +0200

    Better Gitpod env

--- bin/gitpod_restore.sh
+++ bin/gitpod_restore.sh
@@ -3,5 +3,7 @@
 echo "CREATE DATABASE shopware" | mysql -u root --password=root
 mysql -u root --password=root shopware < dump.sql
 
+cp .env.gitpod .env
+
 bin/console system:generate-jwt-secret
 bin/console system:generate-app-secret
